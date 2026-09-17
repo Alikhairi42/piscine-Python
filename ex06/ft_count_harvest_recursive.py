fbdddae002eecf1ef4dcdb594ea9ce34192ba06c
Alikhairi42 <khairiabdelali5@gmail.com>
2026-09-17 12:34:49 +0200

    fix day 06

--- ex06/ft_count_harvest_recursive.py
+++ ex06/ft_count_harvest_recursive.py
@@ -1,5 +1,13 @@
+def count_helper(current_day, max_day):
+    if current_day > max_day:
+        print("Harvest time!")
+        return
+    else:
+        current_day += 1
+        print("Day", current_day)
+        count_helper(current_day, max_day)
+
+
 def ft_count_harvest_recursive():
     days = int(input("Days until harvest: "))
-    for i in range(days, 0, -1):
-        print("Day", i)
-    print("Harvest time!")
+    count_helper(0, days - 1)
