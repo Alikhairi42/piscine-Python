046a6f3cc4b1ee748a0637da9ea2fdd09c9668b5
Alikhairi42 <khairiabdelali5@gmail.com>
2026-09-18 11:29:50 +0200

    ex00

---
+++ Python-Module-01/ex00/ft_garden_intro.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def ft_garden_intro() -> None:
+    print("=== Welcome to My Garden ===")
+    name: str = "ali"
+    height: str = "12cm"
+    age: int = 12
+    print("Plant:", name)
+    print("Height:", height)
+    print("Age:", age)
+    print("=== End of Program ===")
